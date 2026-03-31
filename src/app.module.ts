066e863ad9506b411e876969d87aa668934af022
maximc <maxim.cernetchi@cegeka.com>
2026-03-31 23:13:15 +0300

    feat: add healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { TaskConsumerModule } from './consumers/task-consumer.module';
 import { ProcessorModule } from './processors/processor.module';
 import { RabbitMQModule } from './config/rabbitmq.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     RabbitMQModule,
     TaskConsumerModule,
     ProcessorModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
